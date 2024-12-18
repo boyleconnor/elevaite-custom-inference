e8223b40b3c1d24c6615c94fea9d64dc6521ff73
Connor Boyle <connor.bo@gmail.com>
2024-12-18 12:33:46 -0800

    Add image embedding to ColPali

--- inference.py
+++ inference.py
@@ -2,8 +2,9 @@ import logging
 
 import numpy as np
 import torch
+from PIL import Image
 from colpali_engine import ColPali, ColPaliProcessor
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 from pydantic import BaseModel
 from ray import serve
 from ray.serve import Application
@@ -30,12 +31,14 @@ class CustomDeployment:
         device: str,
     ):
         # Load the model into memory:
-        self.model = ColPali.from_pretrained(
+        self.model: ColPali = ColPali.from_pretrained(
             model_path,
             torch_dtype=torch.bfloat16,
             device_map=device
         ).eval()
-        self.processor = ColPaliProcessor.from_pretrained(model_path)
+        print(f"Type of model: {type(self.model)}")
+        self.processor: ColPaliProcessor = ColPaliProcessor.from_pretrained(model_path)
+        print(f"Type of processor: {type(self.processor)}")
 
     @web_app.post("/infer")
     def infer_query(self, inference_request: InferenceRequest) -> list:
@@ -63,9 +66,26 @@ class CustomDeployment:
         return query_embeddings.tolist()
 
     # TODO: Add other routes with other functionality, if desired
-    @web_app.get("/other_route")
-    def other_functionality(self):
-        raise NotImplementedError()
+    @web_app.post("/infer_image")
+    def infer_image(
+        self,
+        image_files: list[UploadFile] = File([])
+    ):
+        # Process the inputs
+        images = [
+            Image.open(image_file.file).convert("RGB") for image_file in image_files
+        ]
+        batch_images = self.processor.process_images(images).to(self.model.device)
+
+        # Forward pass
+        with torch.no_grad():
+            image_embeddings_tensor = self.model(**batch_images)
+        image_embeddings: np.ndarray = image_embeddings_tensor.cpu().float().numpy()
+
+        # NOTE: `query_embeddings` is a NumPy array, which is not JSON
+        #       serializable. Here we have to convert to a list so that
+        #       FastAPI can convert it JSON.
+        return image_embeddings.tolist()
 
 
 def app_builder(args: dict) -> Application:
